a5764ed1dbfd5b14a76a404176a65274ee8f96b4
bieshan <hpv7xwnpuvm64@yahoo.co.jp>
2020-02-03 18:35:16 +0900

    add: old attacker code

---
+++ attacker/old/99/ddos.go
@@ -0,0 +1,613 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	ch1 := make(chan bool)
+	ch2 := make(chan bool)
+	ch3 := make(chan bool)
+	ch4 := make(chan bool)
+	ch5 := make(chan bool)
+	ch6 := make(chan bool)
+	ch7 := make(chan bool)
+	ch8 := make(chan bool)
+	ch9 := make(chan bool)
+	ch10 := make(chan bool)
+	ch11 := make(chan bool)
+	ch12 := make(chan bool)
+	ch13 := make(chan bool)
+	ch14 := make(chan bool)
+	ch15 := make(chan bool)
+	ch16 := make(chan bool)
+	ch17 := make(chan bool)
+	ch18 := make(chan bool)
+	ch19 := make(chan bool)
+	ch20 := make(chan bool)
+	ch21 := make(chan bool)
+	ch22 := make(chan bool)
+	ch23 := make(chan bool)
+	ch24 := make(chan bool)
+	ch25 := make(chan bool)
+	ch26 := make(chan bool)
+	ch27 := make(chan bool)
+	ch28 := make(chan bool)
+	ch29 := make(chan bool)
+	ch30 := make(chan bool)
+	ch31 := make(chan bool)
+	ch32 := make(chan bool)
+	ch33 := make(chan bool)
+	ch34 := make(chan bool)
+	ch35 := make(chan bool)
+	ch36 := make(chan bool)
+	ch37 := make(chan bool)
+	ch38 := make(chan bool)
+	ch39 := make(chan bool)
+	ch40 := make(chan bool)
+	ch41 := make(chan bool)
+	ch42 := make(chan bool)
+	ch43 := make(chan bool)
+	ch44 := make(chan bool)
+	ch45 := make(chan bool)
+	ch46 := make(chan bool)
+	ch47 := make(chan bool)
+	ch48 := make(chan bool)
+	ch49 := make(chan bool)
+	ch50 := make(chan bool)
+	ch51 := make(chan bool)
+	ch52 := make(chan bool)
+	ch53 := make(chan bool)
+	ch54 := make(chan bool)
+	ch55 := make(chan bool)
+	ch56 := make(chan bool)
+	ch57 := make(chan bool)
+	ch58 := make(chan bool)
+	ch59 := make(chan bool)
+	ch60 := make(chan bool)
+	ch61 := make(chan bool)
+	ch62 := make(chan bool)
+	ch63 := make(chan bool)
+	ch64 := make(chan bool)
+	ch65 := make(chan bool)
+	ch66 := make(chan bool)
+	ch67 := make(chan bool)
+	ch68 := make(chan bool)
+	ch69 := make(chan bool)
+	ch70 := make(chan bool)
+	ch71 := make(chan bool)
+	ch72 := make(chan bool)
+	ch73 := make(chan bool)
+	ch74 := make(chan bool)
+	ch75 := make(chan bool)
+	ch76 := make(chan bool)
+	ch77 := make(chan bool)
+	ch78 := make(chan bool)
+	ch79 := make(chan bool)
+	ch80 := make(chan bool)
+	ch81 := make(chan bool)
+	ch82 := make(chan bool)
+	ch83 := make(chan bool)
+	ch84 := make(chan bool)
+	ch85 := make(chan bool)
+	ch86 := make(chan bool)
+	ch87 := make(chan bool)
+	ch88 := make(chan bool)
+	ch89 := make(chan bool)
+	ch90 := make(chan bool)
+	ch91 := make(chan bool)
+	ch92 := make(chan bool)
+	ch93 := make(chan bool)
+	ch94 := make(chan bool)
+	ch95 := make(chan bool)
+	ch96 := make(chan bool)
+	ch97 := make(chan bool)
+	ch98 := make(chan bool)
+	ch99 := make(chan bool)
+	go func() {
+		request()
+		ch1 <- true
+	}()
+	go func() {
+		request()
+		ch2 <- true
+	}()
+	go func() {
+		request()
+		ch3 <- true
+	}()
+	go func() {
+		request()
+		ch4 <- true
+	}()
+	go func() {
+		request()
+		ch5 <- true
+	}()
+	go func() {
+		request()
+		ch6 <- true
+	}()
+	go func() {
+		request()
+		ch7 <- true
+	}()
+	go func() {
+		request()
+		ch8 <- true
+	}()
+	go func() {
+		request()
+		ch9 <- true
+	}()
+	go func() {
+		request()
+		ch10 <- true
+	}()
+	go func() {
+		request()
+		ch11 <- true
+	}()
+	go func() {
+		request()
+		ch12 <- true
+	}()
+	go func() {
+		request()
+		ch13 <- true
+	}()
+	go func() {
+		request()
+		ch14 <- true
+	}()
+	go func() {
+		request()
+		ch15 <- true
+	}()
+	go func() {
+		request()
+		ch16 <- true
+	}()
+	go func() {
+		request()
+		ch17 <- true
+	}()
+	go func() {
+		request()
+		ch18 <- true
+	}()
+	go func() {
+		request()
+		ch19 <- true
+	}()
+	go func() {
+		request()
+		ch20 <- true
+	}()
+	go func() {
+		request()
+		ch21 <- true
+	}()
+	go func() {
+		request()
+		ch22 <- true
+	}()
+	go func() {
+		request()
+		ch23 <- true
+	}()
+	go func() {
+		request()
+		ch24 <- true
+	}()
+	go func() {
+		request()
+		ch25 <- true
+	}()
+	go func() {
+		request()
+		ch26 <- true
+	}()
+	go func() {
+		request()
+		ch27 <- true
+	}()
+	go func() {
+		request()
+		ch28 <- true
+	}()
+	go func() {
+		request()
+		ch29 <- true
+	}()
+	go func() {
+		request()
+		ch30 <- true
+	}()
+	go func() {
+		request()
+		ch31 <- true
+	}()
+	go func() {
+		request()
+		ch32 <- true
+	}()
+	go func() {
+		request()
+		ch33 <- true
+	}()
+	go func() {
+		request()
+		ch34 <- true
+	}()
+	go func() {
+		request()
+		ch35 <- true
+	}()
+	go func() {
+		request()
+		ch36 <- true
+	}()
+	go func() {
+		request()
+		ch37 <- true
+	}()
+	go func() {
+		request()
+		ch38 <- true
+	}()
+	go func() {
+		request()
+		ch39 <- true
+	}()
+	go func() {
+		request()
+		ch40 <- true
+	}()
+	go func() {
+		request()
+		ch41 <- true
+	}()
+	go func() {
+		request()
+		ch42 <- true
+	}()
+	go func() {
+		request()
+		ch43 <- true
+	}()
+	go func() {
+		request()
+		ch44 <- true
+	}()
+	go func() {
+		request()
+		ch45 <- true
+	}()
+	go func() {
+		request()
+		ch46 <- true
+	}()
+	go func() {
+		request()
+		ch47 <- true
+	}()
+	go func() {
+		request()
+		ch48 <- true
+	}()
+	go func() {
+		request()
+		ch49 <- true
+	}()
+	go func() {
+		request()
+		ch50 <- true
+	}()
+	go func() {
+		request()
+		ch51 <- true
+	}()
+	go func() {
+		request()
+		ch52 <- true
+	}()
+	go func() {
+		request()
+		ch53 <- true
+	}()
+	go func() {
+		request()
+		ch54 <- true
+	}()
+	go func() {
+		request()
+		ch55 <- true
+	}()
+	go func() {
+		request()
+		ch56 <- true
+	}()
+	go func() {
+		request()
+		ch57 <- true
+	}()
+	go func() {
+		request()
+		ch58 <- true
+	}()
+	go func() {
+		request()
+		ch59 <- true
+	}()
+	go func() {
+		request()
+		ch60 <- true
+	}()
+	go func() {
+		request()
+		ch61 <- true
+	}()
+	go func() {
+		request()
+		ch62 <- true
+	}()
+	go func() {
+		request()
+		ch63 <- true
+	}()
+	go func() {
+		request()
+		ch64 <- true
+	}()
+	go func() {
+		request()
+		ch65 <- true
+	}()
+	go func() {
+		request()
+		ch66 <- true
+	}()
+	go func() {
+		request()
+		ch67 <- true
+	}()
+	go func() {
+		request()
+		ch68 <- true
+	}()
+	go func() {
+		request()
+		ch69 <- true
+	}()
+	go func() {
+		request()
+		ch70 <- true
+	}()
+	go func() {
+		request()
+		ch71 <- true
+	}()
+	go func() {
+		request()
+		ch72 <- true
+	}()
+	go func() {
+		request()
+		ch73 <- true
+	}()
+	go func() {
+		request()
+		ch74 <- true
+	}()
+	go func() {
+		request()
+		ch75 <- true
+	}()
+	go func() {
+		request()
+		ch76 <- true
+	}()
+	go func() {
+		request()
+		ch77 <- true
+	}()
+	go func() {
+		request()
+		ch78 <- true
+	}()
+	go func() {
+		request()
+		ch79 <- true
+	}()
+	go func() {
+		request()
+		ch80 <- true
+	}()
+	go func() {
+		request()
+		ch81 <- true
+	}()
+	go func() {
+		request()
+		ch82 <- true
+	}()
+	go func() {
+		request()
+		ch83 <- true
+	}()
+	go func() {
+		request()
+		ch84 <- true
+	}()
+	go func() {
+		request()
+		ch85 <- true
+	}()
+	go func() {
+		request()
+		ch86 <- true
+	}()
+	go func() {
+		request()
+		ch87 <- true
+	}()
+	go func() {
+		request()
+		ch88 <- true
+	}()
+	go func() {
+		request()
+		ch89 <- true
+	}()
+	go func() {
+		request()
+		ch90 <- true
+	}()
+	go func() {
+		request()
+		ch91 <- true
+	}()
+	go func() {
+		request()
+		ch92 <- true
+	}()
+	go func() {
+		request()
+		ch93 <- true
+	}()
+	go func() {
+		request()
+		ch94 <- true
+	}()
+	go func() {
+		request()
+		ch95 <- true
+	}()
+	go func() {
+		request()
+		ch96 <- true
+	}()
+	go func() {
+		request()
+		ch97 <- true
+	}()
+	go func() {
+		request()
+		ch98 <- true
+	}()
+	go func() {
+		request()
+		ch99 <- true
+	}()
+	<-ch1
+	<-ch2
+	<-ch3
+	<-ch4
+	<-ch5
+	<-ch6
+	<-ch7
+	<-ch8
+	<-ch9
+	<-ch10
+	<-ch11
+	<-ch12
+	<-ch13
+	<-ch14
+	<-ch15
+	<-ch16
+	<-ch17
+	<-ch18
+	<-ch19
+	<-ch20
+	<-ch21
+	<-ch22
+	<-ch23
+	<-ch24
+	<-ch25
+	<-ch26
+	<-ch27
+	<-ch28
+	<-ch29
+	<-ch30
+	<-ch31
+	<-ch32
+	<-ch33
+	<-ch34
+	<-ch35
+	<-ch36
+	<-ch37
+	<-ch38
+	<-ch39
+	<-ch40
+	<-ch41
+	<-ch42
+	<-ch43
+	<-ch44
+	<-ch45
+	<-ch46
+	<-ch47
+	<-ch48
+	<-ch49
+	<-ch50
+	<-ch51
+	<-ch52
+	<-ch53
+	<-ch54
+	<-ch55
+	<-ch56
+	<-ch57
+	<-ch58
+	<-ch59
+	<-ch60
+	<-ch61
+	<-ch62
+	<-ch63
+	<-ch64
+	<-ch65
+	<-ch66
+	<-ch67
+	<-ch68
+	<-ch69
+	<-ch70
+	<-ch71
+	<-ch72
+	<-ch73
+	<-ch74
+	<-ch75
+	<-ch76
+	<-ch77
+	<-ch78
+	<-ch79
+	<-ch80
+	<-ch81
+	<-ch82
+	<-ch83
+	<-ch84
+	<-ch85
+	<-ch86
+	<-ch87
+	<-ch88
+	<-ch89
+	<-ch90
+	<-ch91
+	<-ch92
+	<-ch93
+	<-ch94
+	<-ch95
+	<-ch96
+	<-ch97
+	<-ch98
+	<-ch99
+}
+
+func request() {
+	url := "http://raspberrypi.local:8080/ping"
+	for {
+		resp, _ := http.Get(url)
+		_, _ = io.Copy(ioutil.Discard, resp.Body)
+		_ = resp.Body.Close()
+	}
+}
