6926dbd39bb1572027d74c3cb804849365b0e9c1
bieshan <hpv7xwnpuvm64@yahoo.co.jp>
2020-02-03 19:19:39 +0900

    add: no db api

---
+++ raspi/no_db/api_no_db.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type data struct {
+	gorm.Model
+	message string
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
